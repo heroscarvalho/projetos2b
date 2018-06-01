d3dcc44ec87f4b0c95e784a041d2cd6395fb5168
Heros Carvalho <38920661+heroscarvalho@users.noreply.github.com>
2018-06-01 14:24:57 -0300

    Login TestCase

--- src/main/java/testcase/LoginTestCase.java
+++ src/main/java/testcase/LoginTestCase.java
@@ -3,39 +3,47 @@ package testcase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import tasks.LoginTask;
+import tasks.CadastroTask;
 
-
-public class LoginTestCase {
+public class CadastroTestCase {
+	
 	private WebDriver driver;
-	private LoginTask login;
+	private CadastroTask cadastro;
 	
 	
 	
 	@Before
-	public void SetUp() {
+	public void setUp() {
 		WebDriverManager.firefoxdriver().setup();
 		this.driver = new FirefoxDriver();
 		this.driver.get("https://www.grupogreencard.com.br/sysweb/site/loginEstabelecimento");
-		this.login = new LoginTask(driver);	
+		this.driver.manage().window().maximize();
+		this.cadastro = new CadastroTask(driver);
 	}
 	
 	@Test
 	public void testMain() {
-		this.login.PreencherLogin("20474874000198", "10dhsp");
-		this.login.logar();
+		
+		this.cadastro.preencherCadastro("20474874000198", "10dhsp");
+		
+			
+		this.cadastro.Captcha();
+		this.cadastro.enviarFormulario();
+				
 	}
 	
-
-	@After
-	public void tearDown() {
-		//this.driver.quit();
-		
-	}
+//	@After
+//	public void tearDown() {
+//			this.driver.quit();
+//	
+	
+	//}
+}
 
 	
 	
